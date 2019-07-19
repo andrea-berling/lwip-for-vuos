befd7475dc8a05badbbcf9faa96a7963b545f5fb
Andrea Berlingieri <andrea-berling@users.noreply.github.com>
2019-07-19 21:16:10 +0200

    Remove sio.c from the library code
    The sio_tryread symbol would remain undefined in the lwip shared
    library. This is a temporary fix

--- src/include/netif/slipif.h
+++ src/include/netif/slipif.h
@@ -56,7 +56,7 @@
  * If disabled, slipif_poll() polls the serial line (using sio_tryread()).
  */
 #ifndef SLIP_RX_FROM_ISR
-#define SLIP_RX_FROM_ISR 0
+#define SLIP_RX_FROM_ISR 1
 #endif
 
 /** Set this to 1 (default for SLIP_RX_FROM_ISR) to queue incoming packets
