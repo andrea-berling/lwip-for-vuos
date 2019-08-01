d612bf73f7d9e11f62892ed38326eb61ec6967eb
Andrea Berlingieri <andrea-berling@users.noreply.github.com>
2019-08-01 11:39:33 +0200

    Add argument registration for socket event callback
    Modify the socket event addon to use a bitamp to describe which events
    have happened
    
    Modify the code of sockets.c accordingly

--- contrib/addons/socket_event/socket_event.c
+++ contrib/addons/socket_event/socket_event.c
@@ -1,19 +1,21 @@
 #include <addons/socket_event/socket_event.h>
 #include <stdlib.h>
 
-int (*socket_event_callback)(int, int, int, int, int*) = NULL;
+int (*socket_event_callback)(int, unsigned char, void*, int*) = NULL;
+void *socket_event_arg = NULL;
 
-int my_hook_socket_event(int s, int has_recvevent, int has_sendevent, int has_errevent, int *err) {
+int my_hook_socket_event(int s, unsigned char events, int *err) {
     if (socket_event_callback)
-        return socket_event_callback(s, has_recvevent, has_sendevent, has_errevent, err);
+        return socket_event_callback(s, events, socket_event_arg, err);
     else
         return -1;
 }
 
-int register_socket_event_callback(int (*cb)(int, int, int, int, int*)) {
+int register_socket_event_callback(int (*cb)(int, unsigned char, void*, int*), void *arg) {
     if (!socket_event_callback)
     {
         socket_event_callback = cb;
+        socket_event_arg = arg;
         return 0;
     }
     else
